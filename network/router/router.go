132ebeda7bacfaef4cb7ad3695bdc6ea67d727d9
XiaWuSharve <sharve@foxmail.com>
2026-09-10 18:10:24 +0800

    完善router

--- network/router/router.go
+++ network/router/router.go
@@ -1,22 +1,21 @@
 package router
 
 import (
-	"encoding/binary"
 	"errors"
 	"fmt"
 	"log/slog"
 
-	"github.com/XiaWuSharve/whisperly/client"
 	"github.com/XiaWuSharve/whisperly/datas"
 	"github.com/XiaWuSharve/whisperly/mq"
+	"github.com/XiaWuSharve/whisperly/network/conn"
 )
 
 type Router struct {
 	consumer      mq.Consumer[*datas.Send]
-	pool          *client.ConnPool
+	pool          *conn.Pool
 	Err           error
 	storeProducer mq.Producer
-	send2cache    datas.Converter[*datas.Send, *datas.Cache]
+	send2store    datas.Converter[*datas.Send, *datas.Store]
 }
 
 var _ mq.Handler[*datas.Send] = (*Router)(nil)
@@ -29,25 +28,25 @@ func (r *Router) Start() error {
 
 var ErrWaitingRetry = errors.New("send channel is full")
 
-func (r *Router) Handle(frame *datas.Send) error {
-	conn, ok := r.pool.Conns.Get(frame.ConnId)
+func (r *Router) Handle(d *datas.Send) error {
+	handler, ok := r.pool.Get(d.ReceiverId)
 	if !ok {
-		cacheData, err := r.send2cache.Convert(frame)
+		storeData, err := r.send2store.Convert(d)
 		if err != nil {
-			slog.Error("failed to convert send to cache data", "err", err)
-			return nil
+			return fmt.Errorf("failed to convert send to store data: %w", err)
 		}
-		if _, err := r.storeProducer.Enqueue(cacheData); err != nil {
+		if _, err := r.storeProducer.Enqueue(storeData); err != nil {
 			return fmt.Errorf("cannot enqueue store producer: %w", err)
 		}
+		return nil
 	}
+	// r.HeaderBytes[0] = byte(frame.AckStatus)
+	// binary.BigEndian.PutUint64(r.HeaderBytes[1:9], uint64(frame.ConnId))
+	// binary.BigEndian.PutUint32(r.HeaderBytes[9:13], uint32(len(frame.Payload)))
+	slog.Debug("client sending frame")
 
-	r.HeaderBytes[0] = byte(frame.AckStatus)
-	binary.BigEndian.PutUint64(r.HeaderBytes[1:9], uint64(frame.ConnId))
-	binary.BigEndian.PutUint32(r.HeaderBytes[9:13], uint32(len(frame.Payload)))
-	slog.Debug("client sending kcp frame")
 	select {
-	case conn.GetSendChan() <- append(r.HeaderBytes[0:13], frame.Payload...):
+	case handler.SendChan <- d:
 	default:
 		return ErrWaitingRetry
 	}
